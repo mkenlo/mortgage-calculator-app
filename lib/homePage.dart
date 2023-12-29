5e0a8a0b6cd9120fe63434f957e7e11371478f7f
Melanie <mel.lontsi@gmail.com>
2023-12-29 13:52:26 -0800

    refactored slider widget and label into a component

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,7 +1,8 @@
-
 import 'package:flutter/material.dart';
+import "package:intl/intl.dart";
+import 'package:mortgage_calculator_app/widgets/SliderInput.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   final String title;
 
   const HomePage({super.key, required this.title});
@@ -10,97 +11,73 @@ class HomePage extends StatefulWidget{
   State<HomePage> createState() => _HomePageState();
 }
 
+class _HomePageState extends State<HomePage> {
 
-class _HomePageState extends State<HomePage>{
+  double purchasePrice = 100000;
+  double downPayment = 20000;
+  double repaymentTime = 15;
+  double interestRate = 0.03;
 
-  calculate(){ }
+  calculate() {}
 
-  Widget estimateBtn(){
+  Widget estimateBtn() {
     return InkWell(
-      onTap: ()=> calculate(),
-      child: Container(
-        padding: const EdgeInsets.all(24.0),
-    decoration: BoxDecoration(
-    color: Theme.of(context).primaryColor,
-    borderRadius:  BorderRadius.circular(5.0)),
-
-    child: const Text("Get a mortgage quote", style: TextStyle(color: Colors.white)),
-    )
-    );
+        onTap: () => calculate(),
+        child: Container(
+          padding: const EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(5.0)),
+          child: const Text("Get a mortgage quote",
+              style: TextStyle(color: Colors.white)),
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    title: Text(widget.title)
-    ),
-    body: Container(
-      decoration: BoxDecoration(color: Colors.grey.shade100),
-      padding: const EdgeInsets.all(32.0),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-    children:[
-      const Row(
-        children: [ Text("Purchase Price: "), Text("450000")],
-      ),
-      Slider(
-        value: 450000,
-        min: 50000,
-        max: 10000000,
-        label: "450000",
-        onChanged: (double value) {
-
-        },
-      ),
-      const Row(
-        children: [ Text("Down Payment: "), Text("450000")],
-      ),
-      Slider(
-        value: 450000,
-        min: 10000,
-        max: 10000000,
-        label: "450000",
-        onChanged: (double value) {
-
-        },
-      ),
-      const Row(
-        children: [ Text("Repayment Time: "), Text("25 years")],
-      ),
-      Slider(
-        value: 15,
-        max: 30,
-        label: "15",
-        onChanged: (double value) {
-
-        },
-      ),
-      const Row(
-        children: [ Text("Interest Rate: "), Text("3%")],
-      ),
-      Slider(
-        value: 3.99,
-        max: 10,
-        label: "3.99",
-        onChanged: (double value) {
-
-        },
-      ),
-      const Expanded(
-
-          child:  Column(
-        crossAxisAlignment:  CrossAxisAlignment.start,
-        children: [ Text("Loan amount"),
-         Text("315000"),
-         Text("Estimated per month"),
-        Text("1300")]
-      )),
-      Center(child:estimateBtn())
-
-    ]
-    )
-    ));
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(widget.title)),
+        body: Container(
+            decoration: BoxDecoration(color: Colors.grey.shade100),
+            padding: const EdgeInsets.all(32.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              SliderInput(
+                  sliderLabel: "Purchase Price: ",
+                  sliderValue: purchasePrice,
+                  sliderMinValue: 50000,
+                  sliderMaxValue: 5000000,
+                  sliderValueFormat: NumberFormat.currency(symbol:"\$", decimalDigits: 0)),
+                  SliderInput(
+                      sliderLabel: "Down Payment: ",
+                      sliderValue: downPayment,
+                      sliderMinValue: 10000,
+                      sliderMaxValue: 500000,
+                      sliderValueFormat: NumberFormat.currency(symbol:"\$", decimalDigits: 0)),
+                  SliderInput(
+                      sliderLabel: "Repayment Time: ",
+                      sliderValue: repaymentTime,
+                      sliderMinValue: 1,
+                      sliderMaxValue: 30,
+                      sliderValueFormat: NumberFormat("## years")),
+                  SliderInput(
+                      sliderLabel: "Interest Rate: ",
+                      sliderValue: interestRate,
+                      sliderMinValue: 0.01,
+                      sliderMaxValue: 1,
+                      sliderValueFormat: NumberFormat("0.## %")),
+              const Expanded(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                    Text("Loan amount"),
+                    Text("315000"),
+                    Text("Estimated per month"),
+                    Text("1300")
+                  ])),
+              Center(child: estimateBtn())
+            ])));
   }
 }
---
+++ lib/widgets/SliderInput.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' as intl;
+
+class SliderInput extends StatefulWidget {
+  final String sliderLabel;
+  double sliderValue;
+  intl.NumberFormat sliderValueFormat;
+  double sliderMinValue = 10;
+  double sliderMaxValue = 100;
+
+  SliderInput(
+      {super.key,
+      required this.sliderLabel,
+      required this.sliderValue,
+      required this.sliderMinValue,
+      required this.sliderMaxValue,
+      required this.sliderValueFormat});
+  @override
+  State<SliderInput> createState() => _SliderInputState();
+}
+
+class _SliderInputState extends State<SliderInput> {
+  double currentSliderValue = 1;
+
+  @override
+  void initState() {
+    currentSliderValue = widget.sliderValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(children: [
+        Text(widget.sliderLabel),
+        Text(widget.sliderValueFormat.format(currentSliderValue))
+      ]),
+      Slider(
+        value: currentSliderValue,
+        min: widget.sliderMinValue,
+        max: widget.sliderMaxValue,
+        label: currentSliderValue.toString(),
+        divisions: 10,
+        onChanged: (double value) {
+          setState((){
+            currentSliderValue = value;
+          });
+
+        },
+      )
+    ]);
+  }
+}
